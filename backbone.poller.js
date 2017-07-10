a7c9c94252144962c9edaa51b70862016c0c016f
Uzi Kilon <uzi@kilon.org>
2017-07-10 12:54:34 +0300

    bumped version to 1.1.4

--- backbone.poller.js
+++ backbone.poller.js
@@ -1,6 +1,6 @@
 /*!
 (c) 2012 Uzi Kilon, Splunk Inc.
-Backbone Poller 1.1.3
+Backbone Poller 1.1.4
 https://github.com/uzikilon/backbone-poller
 Backbone Poller may be freely distributed under the MIT license.
 */
